f26df7695687bace073dd825f67a7645f2966478
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-15 19:26:17 -0300

    Desafio completo

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        example: 'Using Vue js!'
+        example: 'Using Vue js!',
+        link: 'https://google.com.br'
     },
     methods: {
         changeExample(event){
             this.example = event.target.value
+        },
+        saudacao(){
+            return 'Boa semaninhaaaa :3'
         }
     }
 })
